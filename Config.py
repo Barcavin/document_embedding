1f8d09c0b596b57cb6f3fa788ce8787a539d5b02
Kaiwen Dong <kevindong1994@gmail.com>
2017-12-09 14:13:36 -0600

    doc

---
+++ Config.py
@@ -0,0 +1,10 @@
+vocabulary_size = 20000
+batch_size = 128
+embedding_size = 512
+skip_window = 4
+num_sampled = 256
+num_steps = 1000001
+document_embedding_size = 50 # in paper they used 400
+
+
+dataset_name = 'yelp_p'
